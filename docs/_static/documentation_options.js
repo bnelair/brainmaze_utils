64d27f7f62d7cbdefe16398ffd75432ce92b3d9e
Filip Mivalt <mivalt.filip@mayo.edu>
2025-03-27 16:54:26 -0500

    Docs update (#10)
    * Tests
    
    * Update pypi classifiers
    
    * Fixed classifiers
    
    * Added python 3.12
    
    * fixing version setuptools_scm on github workflows
    
    * fixing version setuptools_scm on github workflows
    
    * fixing version setuptools_scm on github workflows
    
    * Fixing setup.py
    
    * upgrading github workflows
    
    * Fmivalt/v1.0.3 (#7)
    
    * Annotation update for python 3.12
    
    * Fix requirements.txt
    
    * Update docs, typing, formating
    
    * Docs - BrainMaze Update (#9)

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.dev18+g7d729ba.d20250327',
+    VERSION: '1.0.2.dev13+gbf21e4e.d20250327',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
