727d0fca40406f9c70b89e2d154f91782a6e287a
mivalt.filip <mivalt.filip@gmail.com>
2025-11-04 17:38:02 -0600

    fix - cyberpsg loading with non-existing annotation types

--- brainmaze_utils/__init__.py
+++ brainmaze_utils/__init__.py
@@ -4,10 +4,4 @@ Brainmaze utils (:mod:`brainmaze_utils`)
 =======================================
 
 """
-try:
-    from setuptools_scm import get_version
-    __version__ = get_version()
-except LookupError:
-    __version__ = 'dev'  # Fallback version, adjust as appropriate
-
-
+from brainmaze_utils._version import __version__
